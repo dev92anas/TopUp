a832239568cd78bcda073a06337c620637e3952f
Anas Mohammad <dev92.anas@gmail.com>
2024-08-01 00:47:28 +0300

    update pdf file

--- ApiGateways/ocelot/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateways/ocelot/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71cc4cea6c41052d3f0f876daf48ca3e713bb95")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/TopUp/TopUp.API/obj/Debug/net8.0/TopUp.API.AssemblyInfo.cs
+++ Services/TopUp/TopUp.API/obj/Debug/net8.0/TopUp.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopUp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71cc4cea6c41052d3f0f876daf48ca3e713bb95")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopUp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopUp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
